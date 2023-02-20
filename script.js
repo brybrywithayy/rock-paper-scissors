da534990389049c6bfc4d2a9f4df6c13baa5fc93
Bryan Dalina <brybrywithayy@gmail.com>
2023-02-20 11:10:14 -0500

    Add logic of rock, paper, scissors game

--- script.js
+++ script.js
@@ -8,25 +8,27 @@
  *  
  */
 
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
+// const playerSelection = getPlayerChoice().toLowerCase;
+// const computerSelection = getComputerChoice().toLowerCase;
 
 function getComputerChoice() {
     let randomNum = randNum();
     let computerChoice = " ";
     
     if (randomNum === 1) {
-        computerChoice = "Rock";
+        computerChoice = "rock";
         console.log(computerChoice);
     } else if (randomNum === 2) {
-        computerChoice = "Paper";
+        computerChoice = "paper";
         console.log(computerChoice);
     } else if (randomNum === 3) {
-        computerChoice = "Scissors"
+        computerChoice = "scissors"
         console.log(computerChoice);
     } else {
         console.log("Something went wrong");
     }
+
+    return computerChoice;
 }
 
 function randNum() {
@@ -36,16 +38,56 @@ function randNum() {
 }
 
 function getPlayerChoice() {
+    // let playerChoice = prompt("Enter either rock, paper, or scissors: ").toLowerCase;
+    // return playerChoice;
     return "rock";
 }
 
-function playRound(playerSelection, computerSelection) {
-    
+function playRound() {
+    // call functions to get selections
+    let playerSelection = getPlayerChoice().toLowerCase;
+    let computerSelection = getComputerChoice().toLowerCase;
+
+    // win condition messages
+    let rockWinsMessage = "Rock beats scissors";
+    let paperWinsMessage = "Paper beats rock";
+    let scissorsWinMessage = "Scissors beat paper";
+    let playerWins = " - YOU WIN!";
+    let computerWins = " - computer wins";
+    let tieMessage = "Tie! Try again";
+
+    // logic for sorting type precedence
+    if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            console.log(paperWinsMessage + computerWins);
+        } else if (computerSelection === "scissors") {
+            console.log(rockWinsMessage + playerWins);
+        } else if (computerSelection === "rock") {
+            console.log(tieMessage);
+        }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            console.log(paperWinsMessage + playerWins);
+        } else if (computerSelection === "scissors") {
+            console.log(scissorsWinMessage + computerWins);
+        } else if (computerSelection === "paper") {
+            console.log(tieMessage);
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            console.log(rockWinsMessage + computerWins);
+        } else if (computerSelection === "paper") {
+            console.log(scissorsWinMessage + playerWins);
+        } else if (computerSelection === "scissors") {
+            console.log(tieMessage);
+        }
+    }
 }
 
 function game() {
     for (let i=0; i<5; i++) {
-        playRound(getPlayerChoice(), getComputerChoice());
-        
+        playRound();
     }
 }
+
+playRound();
